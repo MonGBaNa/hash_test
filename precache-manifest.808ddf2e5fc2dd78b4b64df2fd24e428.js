e991e22e343898e7baaa186e5835c8aa2c259314
MonG_Bana <jjasu0826@gmail.com>
2020-05-04 17:22:37 +0900

    Updates

---
+++ precache-manifest.808ddf2e5fc2dd78b4b64df2fd24e428.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17c6a70e1711073b08da2c7c7f4ee2ae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f63cede49fec1302bf8b",
+    "url": "/static/js/2.6498bcef.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.6498bcef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c81b053b6dccf831025d",
+    "url": "/static/js/main.9d4088a5.chunk.js"
+  },
+  {
+    "revision": "a6c865dc91e33a9b45e3",
+    "url": "/static/js/runtime-main.1da8022e.js"
+  }
+]);
